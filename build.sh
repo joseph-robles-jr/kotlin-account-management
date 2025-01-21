c5b0a479fd3c8b1c17d7489344fe72f7a92986f3
joseph-robles-jr <rjosepha@byui.edu>
2025-01-21 13:00:25 -0700

    fixed build.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kotlinc -classpath src -d main.jar src/com/bank/Main.kt src/com/bank/Account.kt src/com/bank/SavingsAccount.kt src/com/bank/Bank.kt src/com/bank/checkingAccount.kt -include-runtime
+kotlinc -classpath src -d main.jar src/com/bank/Main.kt src/com/bank/Account.kt src/com/bank/SavingsAccount.kt src/com/bank/Bank.kt src/com/bank/checkingAccount.kt src/com/bank/FillerAccount.kt -include-runtime
